c2c0311823bd3bd1d393c806cee967f6cd549b27
Kieran Prasch <kieranprasch@gmail.com>
2018-07-10 08:43:24 -0700

    Add versioning metadata and bumpversion config

---
+++ bytestring_splitter/__about__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function
+
+__all__ = [
+    "__title__", "__summary__", "__version__", "__author__", "__license__",
+]
+
+__title__ = "bytestringSplitter"
+
+__summary__ = 'Among the simplest imaginable ways to serialize: concatenate and split.',
+
+__version__ = "0.1.0-alpha.0"
+
+__author__ = "NuCypher"
+
+# __license__ = "GNU General Public License, Version 3"
+
+# __copyright__ = ""
