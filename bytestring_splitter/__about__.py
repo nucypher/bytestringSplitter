e8c2c0f18707fb7d3ad3ce0aaf368cd73ef34a7d
jMyles <justin@justinholmes.com>
2019-10-11 16:12:59 +0900

    Bump version: 1.1.0 → 1.2.0

--- bytestring_splitter/__about__.py
+++ bytestring_splitter/__about__.py
@@ -10,7 +10,7 @@ __url__ = "https://github.com/nucypher/bytestringSplitter"
 
 __summary__ = 'Among the simplest imaginable ways to serialize: concatenate and split.'
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 __author__ = "NuCypher"
 
