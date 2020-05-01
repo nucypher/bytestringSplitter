41115d24f338a3655758db851325d894fd1991dc
Kieran R. Prasch <kieranprasch@gmail.com>
2020-04-30 22:07:15 -0700

    work splittables into a composite strategy

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -1,29 +1,40 @@
-from hypothesis import given
+from hypothesis import given, example
 from hypothesis import strategies as st
+from hypothesis.strategies import composite
 
 from bytestring_splitter import BytestringSplitter
 
 
 @given(st.binary(min_size=1))
+@example(b'hello world')
 def test_tiny_splitter_properties(bytestring):
     splitter = BytestringSplitter(len(bytestring))
     assert splitter(bytestring) == [bytestring]
 
 
-@given(st.data())
-def test_triad_splitter_properties(data):
+@composite
+def bytesting_and_trid_pattern(draw):
 
     # Hypothesis
-    bytestring = data.draw(st.binary(min_size=3))
+    bytestring = draw(st.binary(min_size=3))
     near_the_end = len(bytestring)-1
-    first_member_length = data.draw(st.integers(min_value=1, max_value=near_the_end))
+    first_member_length = draw(st.integers(min_value=1, max_value=near_the_end))
     remainder = len(bytestring) - first_member_length
-    second_member_length = data.draw(st.integers(min_value=1, max_value=remainder))
+    second_member_length = draw(st.integers(min_value=1, max_value=remainder))
 
     # Setup Pattern
     header_pattern = (first_member_length, second_member_length)
     pattern = (*header_pattern, len(bytestring)-sum(header_pattern))
-    first_position, second_position = header_pattern[0], sum(header_pattern)
+
+    return bytestring, pattern
+
+
+@given(bytesting_and_trid_pattern())
+@example((b'hello world', (5, 1, 5)))
+def test_triad_splitter_properties(bytestring_and_pattern):
+
+    bytestring, pattern = bytestring_and_pattern
+    first_position, second_position = pattern[0], pattern[0]+pattern[1]
 
     # Splitter
     splitter = BytestringSplitter(*pattern)
